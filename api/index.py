5bac349fb6ca1395fe0efd0f98fc7c892e1bac7f
晏长城 <742450622@qq.com>
2024-07-05 13:58:19 +0800

    完善项目

--- api/index.py
+++ api/index.py
@@ -1,71 +1,11 @@
-from flask import Flask, request, jsonify
-from requests import HTTPError
-from werkzeug.utils import secure_filename
-import os
+from flask import Flask
 
+app = Flask(__name__)
 
-from utils import utils
-
-from utils  import getFileData
-
-
-app = Flask(__name__,static_folder='dist')
 @app.route('/')
-def index():
-    return app.send_static_file('index.html')
+def home():
+    return 'Hello, World!'
 
-
-# 设置上传文件的保存目录
-UPLOAD_FOLDER = os.path.join(os.getcwd(), 'upload')
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 200 * 1024 * 1024
-
-@app.route('/api/content', methods=['POST'])
-def upload_files():
-
-    print(request.files)
-    print(len(request.files))
-
-    print(request.form)
-
-    try:
-
-        if request.form["token"] is None or request.form["model"] == '':
-            return jsonify({'code':500,'message': 'not token'}), 200
-
-        if 'file' not in request.files and not 'text' in request.form:
-            return jsonify({'code':500,'message': 'not file or text'}), 200
-        
-        text =  request.form['text']
-        api_key = request.form["token"]
-        model = request.form["model"]
-
-        if 'file'  in request.files:
-            promot =  request.form['text']
-            if not promot:
-                return jsonify({'code':500,'message': 'not file or text'}), 200
-            
-            file = request.files['file']
-            if file.filename == '':
-                return jsonify({'code':500,'message': 'no file  upload', 'data': None}), 200
-            file = request.files['file']
-            file_path = ""
-            if file:
-                filename = secure_filename(file.filename)
-                file_path = os.path.join(UPLOAD_FOLDER, filename)
-                file.save(file_path)
-
-            res = getFileData.getFileData(filePaths=[file_path],promot=promot,api_key=api_key,model=model)
-
-            return jsonify({"code":200,'message': 'fetch Success', 'data': res}), 200
-        
-        else:
-            text =  request.form['text']
-            print(text)
-            res = utils.fetch_text_bychatgpt(text,api_key=api_key,model=model)
-            return jsonify({"code":200,'message': 'fetch Success', 'data': res}), 200
-        
-    except HTTPError as error:
-            return jsonify({"code":500,'message': error, 'data': None}), 200    
-
+@app.route('/about')
+def about():
+    return 'About'
